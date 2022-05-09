5fe08d33357646b1519cdbf01df85b766473384d
haruluya <2198645565@qq.com>
2022-05-09 15:22:44 +0800

    update templates.

--- tifa-ui/src/pages/GoodDetail/index.vue
+++ tifa-ui/src/pages/GoodDetail/index.vue
@@ -36,25 +36,60 @@
                         <div class="labels">
                             <div class="label">
                                 原价格：
+                                <div class="prePrice">￥9999</div>
                             </div>
                             <div class="label">
                                 现价格：
+                                <div class="nowPrice">￥9999
+                                    <el-tag class="ml-2" type="danger">特价</el-tag>
+                                    <el-tag class="ml-2" type="danger">满9999减1</el-tag>
+                                </div>
                             </div>
+
                         </div>
-                        <div class="price">                    
-                            <div class="prePrice">￥9999</div>
-                            <div class="prePrice">￥9999</div>
+                        <div class="sales">
+                            <el-divider direction="vertical"></el-divider>
+                            累计销量：
+                            <div class="salesNum">
+                                888
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="color"> </div>
-                <div class="color"> </div>
-                <div class="amount"></div>
-                <div class="addShopCart"></div>
+                <div class="location">
+                    <span>配送至：</span>
+                    <div class="locationCas">
+                        <el-cascader
+                        v-model=" locationValue"
+                        :options="options"
+                        :props="props"
+                        @change="handleChange"
+                        />
+                    </div>
+                </div>
+                <div class="color">
+                    <span>颜色：</span>
+                    <el-radio v-model="goodColor" label="1"  border>黄色</el-radio>
+                    <el-radio v-model="goodColor" label="2"  border>蓝色</el-radio>
+                </div>
+                <div class="amount">
+                    <span>数量：</span>
+                    <el-input-number
+                        v-model="goodNum"
+                        :min="1"
+                        :max="10"
+                        controls-position="right"
+                        @change="handleChange"
+                    />
+                </div>
+                <el-divider></el-divider>
+                <div class="addShopCart">
+                    <el-icon><shopping-cart-full /></el-icon>加入购物车
+                </div>
             </div>
         </div>
         <div class="simRec">
-
+            <RecPanel title="基于Item-CF的商品相似推荐"/>
         </div>
         <div class="goodImgs">
 
@@ -63,10 +98,296 @@
 </template>
 <script>
 import HomeHeader from '../ShopHome/HomeHeader'
+import RecPanel from '../ShopHome/RecPanel'
+// 级联地址选项。
+const props = {
+  expandTrigger: 'hover',
+}
+const handleChange = (value) => {
+  console.log(value)
+}
+const options = [
+  {
+    value: 'guide',
+    label: 'Guide',
+    children: [
+      {
+        value: 'disciplines',
+        label: 'Disciplines',
+        children: [
+          {
+            value: 'consistency',
+            label: 'Consistency',
+          },
+          {
+            value: 'feedback',
+            label: 'Feedback',
+          },
+          {
+            value: 'efficiency',
+            label: 'Efficiency',
+          },
+          {
+            value: 'controllability',
+            label: 'Controllability',
+          },
+        ],
+      },
+      {
+        value: 'navigation',
+        label: 'Navigation',
+        children: [
+          {
+            value: 'side nav',
+            label: 'Side Navigation',
+          },
+          {
+            value: 'top nav',
+            label: 'Top Navigation',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'component',
+    label: 'Component',
+    children: [
+      {
+        value: 'basic',
+        label: 'Basic',
+        children: [
+          {
+            value: 'layout',
+            label: 'Layout',
+          },
+          {
+            value: 'color',
+            label: 'Color',
+          },
+          {
+            value: 'typography',
+            label: 'Typography',
+          },
+          {
+            value: 'icon',
+            label: 'Icon',
+          },
+          {
+            value: 'button',
+            label: 'Button',
+          },
+        ],
+      },
+      {
+        value: 'form',
+        label: 'Form',
+        children: [
+          {
+            value: 'radio',
+            label: 'Radio',
+          },
+          {
+            value: 'checkbox',
+            label: 'Checkbox',
+          },
+          {
+            value: 'input',
+            label: 'Input',
+          },
+          {
+            value: 'input-number',
+            label: 'InputNumber',
+          },
+          {
+            value: 'select',
+            label: 'Select',
+          },
+          {
+            value: 'cascader',
+            label: 'Cascader',
+          },
+          {
+            value: 'switch',
+            label: 'Switch',
+          },
+          {
+            value: 'slider',
+            label: 'Slider',
+          },
+          {
+            value: 'time-picker',
+            label: 'TimePicker',
+          },
+          {
+            value: 'date-picker',
+            label: 'DatePicker',
+          },
+          {
+            value: 'datetime-picker',
+            label: 'DateTimePicker',
+          },
+          {
+            value: 'upload',
+            label: 'Upload',
+          },
+          {
+            value: 'rate',
+            label: 'Rate',
+          },
+          {
+            value: 'form',
+            label: 'Form',
+          },
+        ],
+      },
+      {
+        value: 'data',
+        label: 'Data',
+        children: [
+          {
+            value: 'table',
+            label: 'Table',
+          },
+          {
+            value: 'tag',
+            label: 'Tag',
+          },
+          {
+            value: 'progress',
+            label: 'Progress',
+          },
+          {
+            value: 'tree',
+            label: 'Tree',
+          },
+          {
+            value: 'pagination',
+            label: 'Pagination',
+          },
+          {
+            value: 'badge',
+            label: 'Badge',
+          },
+        ],
+      },
+      {
+        value: 'notice',
+        label: 'Notice',
+        children: [
+          {
+            value: 'alert',
+            label: 'Alert',
+          },
+          {
+            value: 'loading',
+            label: 'Loading',
+          },
+          {
+            value: 'message',
+            label: 'Message',
+          },
+          {
+            value: 'message-box',
+            label: 'MessageBox',
+          },
+          {
+            value: 'notification',
+            label: 'Notification',
+          },
+        ],
+      },
+      {
+        value: 'navigation',
+        label: 'Navigation',
+        children: [
+          {
+            value: 'menu',
+            label: 'Menu',
+          },
+          {
+            value: 'tabs',
+            label: 'Tabs',
+          },
+          {
+            value: 'breadcrumb',
+            label: 'Breadcrumb',
+          },
+          {
+            value: 'dropdown',
+            label: 'Dropdown',
+          },
+          {
+            value: 'steps',
+            label: 'Steps',
+          },
+        ],
+      },
+      {
+        value: 'others',
+        label: 'Others',
+        children: [
+          {
+            value: 'dialog',
+            label: 'Dialog',
+          },
+          {
+            value: 'tooltip',
+            label: 'Tooltip',
+          },
+          {
+            value: 'popover',
+            label: 'Popover',
+          },
+          {
+            value: 'card',
+            label: 'Card',
+          },
+          {
+            value: 'carousel',
+            label: 'Carousel',
+          },
+          {
+            value: 'collapse',
+            label: 'Collapse',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'resource',
+    label: 'Resource',
+    children: [
+      {
+        value: 'axure',
+        label: 'Axure Components',
+      },
+      {
+        value: 'sketch',
+        label: 'Sketch Templates',
+      },
+      {
+        value: 'docs',
+        label: 'Design Documentation',
+      },
+    ],
+  },
+]
 export default {
     name: 'gooddetail',
     components:{
         HomeHeader,
+        RecPanel
+    },
+    data() {
+        return {
+            locationValue:'',
+            options,
+            props,
+            goodColor: '1',
+            goodNum : 1,
+        }
     },
 
 }
@@ -77,7 +398,6 @@ export default {
     }
     .topInfo{
         width: 1200px;
-        height: 600px;
         // background-color: white;
         margin: 0 auto; 
         display: flex;
@@ -141,14 +461,86 @@ export default {
                     display: flex;
                     padding: 10px;
                     padding-left: 20px;
-                    .label{
+                    .labels{
                         font-size: 18px;
-                        .labels{
+                        .label{
+                            display: flex;
+                            margin-top: 20px;
+                            color: #999999;
+                            .prePrice{
+                                color: #7049ff;
+                                font-size: 20px;
+                                font-weight: bold;
+                            }
+                            .nowPrice{
+                                font-size: 25px;
+                                font-weight: bold;
+                                color: #e4393c;
+                                transform: translateY(-5px);
+                                .el-tag{
+                                    margin-left: 5px;
+                                    transform: translateY(-5px);
+                                }
+                            }
+                        }
+
+                    }
+                    .sales{
+                        margin-left: 150px;
+                        color: #999999;
+                        font-size: 18px;
+                        .salesNum{
                             margin-top: 10px;
+                            color: #007fc2;
                         }
                     }
                 }
             }
+            .location{
+                margin-top: 20px;
+                display: flex;
+                font-size: 18px;
+                color: #c8c8c8;
+                .locationCas{
+                    margin-left: 15px;
+                    transform: translateY(-3px);
+                }
+            }
+            .color{
+                margin-top: 20px;
+                display: flex;
+                font-size: 18px;
+                color: #c8c8c8;
+                .el-radio{
+                    margin-right: 10px;
+                }
+            }
+            .amount{
+                margin-top: 20px;
+                display: flex;
+                font-size: 18px;
+                color: #c8c8c8;
+            }
+            .addShopCart{
+                font-size: 23px;
+                font-weight: bold;
+                display: flex;
+                margin-top: 20px;
+                color: white;
+                width: 150px;
+                padding: 8px;
+                border-radius: 5px;
+                background-color: #df3033;
+                &:hover{
+                    transform: scale(1.01);
+                    cursor: pointer;
+                }
+            }
         }
+    }
+    .simRec{
+        width: 1400px;
+        margin: 0 auto;
+        margin-top: 50px;
     }
 </style>
