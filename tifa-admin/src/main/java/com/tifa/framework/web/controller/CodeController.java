20283a2328412fe682fce175df7618e64889aef5
haruluya <2198645565@qq.com>
2022-05-31 10:20:14 +0800

    finish tifa 1.4.0

---
+++ tifa-admin/src/main/java/com/tifa/framework/web/controller/CodeController.java
@@ -0,0 +1,4 @@
+package com.tifa.framework.web.controller;
+
+public class CodeController {
+}
---
+++ tifa-admin/src/main/java/com/tifa/framework/web/util/ConstansWxUtils.java
@@ -0,0 +1,25 @@
+package com.tifa.framework.web.util;
+
+//实现InitializingBean调用afterPropertiesSet()方法进行赋值
+@Component
+public class ConstansWxUtils implements InitializingBean {
+    @Value("${wx.open.app_id}")
+    private String appId;
+
+    @Value("${wx.open.app_secret}")
+    private String appSecret;
+
+    @Value("${wx.open.redirect_url}")
+    private String redirectUrl;
+
+    public static String WX_OPEN_APP_ID;
+    public static String WX_OPEN_APP_SECRET;
+    public static String WX_OPEN_REDIRECT_URL;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        WX_OPEN_APP_ID = appId;
+        WX_OPEN_APP_SECRET = appSecret;
+        WX_OPEN_REDIRECT_URL = redirectUrl;
+    }
+}
